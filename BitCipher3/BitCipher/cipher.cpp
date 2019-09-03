686deca6bc2ea7f149c2b765b66c02475d49ba16
Kang Shinil <rtd990@naver.com>
2019-09-03 11:37:20 +0900

    Implement a few of decode function
    Remove magic number that I put on functions for test.

--- BitCipher3/BitCipher/cipher.cpp
+++ BitCipher3/BitCipher/cipher.cpp
@@ -10,9 +10,10 @@ Creation date: 09/02/2019
 - End Header ----------------------------------------------------------------
 */
 
-#include <vector>
-#include <string>
+#include <vector>	// std::vector
+#include <string>	// std::string
 #include "cipher.h"
+#include <iostream> // std::cout
 
 namespace
 {
@@ -35,9 +36,35 @@ int numBits(int value)
 	return requiredBits;
 }
 
+char TryGetHuffmanChar(int groupBit, int indexBit)
+{
+	if (groupBit < 0 || groupBit >= NUMGROUPS)
+	{
+		throw "GroupBit is out of range!";
+	}
+	if (indexBit < 0 || indexBit >= int(groups[groupBit].length()))
+	{
+		throw "IndexBit is out of range!";
+	}
+	return groups[groupBit].at(indexBit);
+}
+
+// If value is a hover value, make push it to ground.
+int GetCountToFloorValue(int value)
+{
+	int count = 0;
+	while (value % 2 == 0)
+	{
+		value >>= 1;
+		++count;
+	}
+	return count;
+}
+
 char GetBinaryValue(char bits, char mask)
 {
-	return (bits & mask) >> numBits(mask);
+	int count = GetCountToFloorValue(mask);
+	return (bits & mask) >> count;
 }
 
 // Return decoded value given character.
@@ -59,7 +86,7 @@ std::vector<char> encode(std::string uncompressed)
 	std::vector<char> compressed;
 	for (const auto & element : uncompressed)
 	{
-			compressed.push_back(GetEncodedChar(element));
+		compressed.push_back(GetEncodedChar(element));
 	}
 	return compressed;
 }
@@ -68,28 +95,55 @@ std::vector<char> encode(std::string uncompressed)
 std::string decode(std::vector<char> compressed)
 {
 	std::string str;
-	char mask;
+	str.clear();
 
+	char mask;
 	for (const auto & element : compressed)
 	{
-		int result = GetBinaryValue(element, 0xA0);
+		//int result = GetBinaryValue(element, 0xA0);
+		//switch (result)
+		//{
+		//case 0:
+		//	mask = 0x20;
+		//	break;
+		//case 1:
+		//	mask = 0x30;
+		//	break;
+		//case 2:
+		//	mask = 0x38;
+		//	break;
+		//case 3:
+		//	mask = 0x3C;
+		//	break;
+		//default:
+		//	break;
+		//}
+
+		char result = GetBinaryValue(element, 0x18);
 		switch (result)
 		{
-		case 0:
-			mask = 0x20;
-			break;
-		case 1:
-			mask = 0x30;
-			break;
-		case 2:
-			mask = 0x38;
-			break;
-		case 3:
-			mask = 0x3C;
+		case 0b10:
+			mask = 0x07;
 			break;
 		default:
+			mask = 0x00;
 			break;
 		}
+		char c = '\0';
+		int index = GetBinaryValue(element, mask);
+		try
+		{
+			c = TryGetHuffmanChar(result, index);
+		}
+		catch (const std::string& str)
+		{
+			std::cout << str;
+		}
+		catch(const char* str)
+		{
+			std::cout << str;
+		}
+		str += c;
 	}
 
 	return str;
